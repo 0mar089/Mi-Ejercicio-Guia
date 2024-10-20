da57af66f1e7d4cba6c0c8ad31cf1dbd4fd207b9
Omar089 <omar.boury@estudiantat.upc.edu>
2024-10-20 21:17:27 +0200

    Version con exclusion mutua

--- clienteEjercicioGuia/WindowsFormsApplication1/Form1.cs
+++ clienteEjercicioGuia/WindowsFormsApplication1/Form1.cs
@@ -25,7 +25,31 @@ namespace WindowsFormsApplication1
            
         }
 
-   
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9000);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex) {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
 
@@ -103,8 +127,32 @@ namespace WindowsFormsApplication1
 
         }
 
-   
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Mensaje de desconexión
+            string mensaje = "0/";
 
-     
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            // Nos desconectamos
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Pedir numero de servicios realizados
+            string mensaje = "4/";
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+            contLbl.Text = mensaje;
+        }
     }
 }
